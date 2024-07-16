dca1a058f2eaf74074e2a225c099d06e34a0059c
angelo-bernardo <angelo.bernard005@gmail.com>
2024-07-15 21:01:25 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,27 @@
+public class Variaveis {
+    public static void main(String[] args) throws Exception {
+        int varInt = 2; // declaração da variável varInt, do tipo int, com valor recebido 2
+        System.out.println("Variável do tipo int (inteiro): " + varInt);
+
+        Integer varInteger = 3; // declaração da variável varInteger, do tipo integer, com valor recebido 3
+        System.out.println("Variável do tipo Integer (inteiro): " + varInteger);
+
+        String varString = "texto"; // declaração da variável varString, do tipo string, com valor recebido "texto"
+        System.out.println("Variável do tipo String (texto): " + varString);
+
+        boolean varBoolean = true; // declaração da variável varBoolean, do tipo boolean, com valor recebido true
+        System.out.println(varBoolean);
+
+        byte varByte = 1;  // declaração da variável varByte, do tipo byte, com valor recebido 1
+        System.out.println(varByte);
+
+        char varChar = 'r';  // declaração da variável varChar, do tipo char, com valor recebido 'r'
+        System.out.println(varChar);
+
+        float varFloat = 19.99f; // declaração da variável varFloat, do tipo float, com valor recebido 19.99f
+        System.out.println(varFloat);
+
+        double varDouble = 0.263568954; // declaração da variável varDouble, do tipo double, com valor recebido 0.263568954
+        System.out.println(varDouble);
+    }
+}
