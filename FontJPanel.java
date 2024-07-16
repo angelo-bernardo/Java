a4b6013251d3a167ee7a319fc1313adda0eef3e3
angelo-bernardo <angelo.bernard005@gmail.com>
2024-07-15 21:46:17 -0300

    Add files via upload

---
+++ FontJPanel.java
@@ -0,0 +1,20 @@
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class FontJPanel extends JPanel
+{
+   @Override
+   public void paintComponent(Graphics g)
+   {
+      super.paintComponent(g); 
+
+      g.fillRect(20, 40, 100, 50);
+      g.setColor(Color.BLUE);
+
+      g.setFont(new Font("Arial", Font.ITALIC, 22));
+      g.drawString("Senac", 22, 70);
+   } 
+}
+
